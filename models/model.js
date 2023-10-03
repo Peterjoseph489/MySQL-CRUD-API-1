1c548b61b0d86947bdc2924b0da3b05349d63bd6
Joseph <josephpeter489@gmail.com>
2023-10-03 12:53:14 +0100

    MySQL getting Interesting

---
+++ models/model.js
@@ -0,0 +1,33 @@
+const { sequelize, connectDB } = require('../config/dbConfig')
+const { DataTypes } = require('sequelize')
+
+
+const userModel = sequelize.define('Task', {
+    content: {
+        types: DataTypes.STRING,
+        validate: {
+            max: 150
+        }
+    },
+    description: {
+        type: DataTypes.TEXT
+    },
+    isComplete: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    timestamps: true
+});
+
+
+(async () => {
+    try {
+        await sequelize.sync();
+        console.log('Database synced successfully');
+    } catch (error) {
+        console.error('Error syncing database:', error);
+    }
+})();
+
+module.exports = userModel
